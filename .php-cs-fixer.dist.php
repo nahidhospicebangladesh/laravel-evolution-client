1a9fde07b551d1311cce25d66932b94e7996aae6
Samuel Terra <samuelterra22@hotmail.com>
2025-04-15 05:25:33 -0300

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,26 +12,30 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR2'                             => true,
-        'array_syntax'                      => ['syntax' => 'short'],
-        'ordered_imports'                   => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports'                 => true,
-        'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline'       => true,
-        'phpdoc_scalar'                     => true,
-        'unary_operator_spaces'             => true,
-        'binary_operator_spaces'            => true,
-        'blank_line_before_statement'       => [
+        '@PSR2'                              => true,
+        'array_syntax'                       => ['syntax' => 'short'],
+        'ordered_imports'                    => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports'                  => true,
+        'not_operator_with_successor_space'  => true,
+        'trailing_comma_in_multiline'        => true,
+        'phpdoc_scalar'                      => true,
+        'unary_operator_spaces'              => true,
+        'binary_operator_spaces'             => [
+            'default'   => 'align',
+            'operators' => [
+                '=>' => 'align',
+            ],
+        ],
+        'blank_line_before_statement'        => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing'    => true,
-        'phpdoc_var_without_name'           => true,
-        'method_argument_space'             => [
+        'phpdoc_single_line_var_spacing'     => true,
+        'phpdoc_var_without_name'            => true,
+        'method_argument_space'              => [
             'on_multiline'                     => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'single_trait_insert_per_statement' => true,
-
+        'single_trait_insert_per_statement'  => true,
         'braces'                             => [
             'position_after_functions_and_oop_constructs' => 'next',
             'position_after_anonymous_constructs'         => 'same',
@@ -61,8 +65,7 @@ return (new PhpCsFixer\Config())
         'phpdoc_separation'                  => true,
         'single_blank_line_at_eof'           => true,
         'fully_qualified_strict_types'       => true,
-
-        'psr_autoloading' => [
+        'psr_autoloading'                    => [
             'dir' => null,
         ],
     ])
